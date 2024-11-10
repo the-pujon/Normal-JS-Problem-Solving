069c1cf0c1ce7e7e93bd14d601d1ffd2018127c4
Pujon Das Auvi <auvipujondas@gmail.com>
2024-11-10 21:51:36 +0600

    2. Palindrome Check.js updated

--- 2. Palindrome Check.js
+++ 2. Palindrome Check.js
@@ -1,4 +1,6 @@
 const isPalindrome = (str) => {
+  //time complexity is O(n) and space complexity is O(n)
+  str = str.toLowerCase();
   let reverse = "";
   for (let i = str.length - 1; i >= 0; i--) {
     reverse += str[i];
