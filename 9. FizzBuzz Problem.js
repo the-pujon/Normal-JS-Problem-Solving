2fb17bf040081a6de8e0bc76bdb429b8971a2c41
Pujon Das Auvi <auvipujondas@gmail.com>
2024-11-10 22:39:15 +0600

    9. FizzBuzz Problem solved

---
+++ 9. FizzBuzz Problem.js
@@ -0,0 +1,14 @@
+const fizzBuzz = (n) => {
+  const result = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) result.push("FizzBuzz");
+    else if (i % 3 === 0) result.push("Fizz");
+    else if (i % 5 === 0) result.push("Buzz");
+    else result.push(i);
+  }
+  return result;
+};
+
+console.log(fizzBuzz(15));
+
+//here time complexity is O(n) and space complexity is O(n)
